80f6990a4208cc38ce28381b4ac8efffe3f7e1a8
pritam8250666100 <49182532+pritam8250666100@users.noreply.github.com>
2019-04-08 16:28:58 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,49 @@
+
+def joy():
+    print("joyful")
+
+joy()
+
+
+def func(name):
+    print("high",name)
+
+func("pritam")
+
+
+
+def si(p,t,r):
+    s=(p*t*r)/100
+    return s
+
+p=float(input("enter principle: "))
+r=float(input("enter rate: "))
+t=float(input("enter time in years: "))
+print("the simple interest is:",si(p,t,r))
+
+
+
+
+##def sum(a,b):
+##    return(a+b)
+##
+##
+##print("the sum is:",sum(10,4))
+
+
+def hh(name,message):
+    print("printing the message with",name,"and",message)
+
+hh(name=(input("enter a name")),message=(input("enter a message")))
+
+
+
+
+
+def si(p,t,r):
+    return((p*t*r)/100)
+
+print("the simple interest is:",si(t=10,r=10,p=1900))
+
+
+
---
+++ if and for .py
@@ -0,0 +1,157 @@
+##a=int(input("enter a: "))
+##b=int(input("enter b: "))
+##c=int(input("enter c: "))
+##if a>b and a>c:
+##    print("a is greater")
+##elif a>b and a>c:
+##    print("b is greater")
+##else:
+##    print("c is greater")
+##
+##
+##a=int(input("enter a no. "))
+##if a==10:
+##    print("number entered is 10")
+##elif a==50:
+##    print("number entered is 50")
+##elif a==100:
+##    print("number entered is 100")
+##
+##
+##a=int(input("enter your marks: "))
+##if a>=90 and a<=100:
+##    print("your grade is A")
+##elif a>=80 and a<90:
+##    print("your grade is B")
+##elif a>=60 and a<80:
+##    print("your grade is C")
+##elif a>=50 and a<60:
+##    print("your grade is D")
+##elif a>=40 and a<50:
+##    print("your grade is E")
+##elif a<40:
+##    print("your grade is F")
+##
+##a=(input("enter a character: "))
+##b=a.lower()
+##if b=='a' and b=='e' and b=='i' and b=='o' and b=='u':
+##    print("its a vowel")
+##else:
+##    print("consonant")
+##
+##a=int(input("enter a year: "))
+##if ((a%400==0) or (a%100!=0)) and (a%4==0): 
+##    print("leap year")
+##else:
+##    
+##    print("not a leap year")
+##
+##a=str(input("enter a word: "))
+##b=""
+##for i in a:
+##    b=i+b
+##if(a==b):
+##   print("palindrome")
+##else:
+##   print("not palindrome")
+##    
+##
+##
+##x=int(input("enter a no.: "))
+##for i in range(1,11):
+##    b=x*i
+##    print(x,"x",i,"=",b)
+##
+##
+##n=int(input("input no. of rows : "))
+##i,j=0,0
+##for i in range(0,n):
+##    print()
+##    for j in range(0,i+1):
+##        print("*",end="")
+##
+##for i in range(0,5):
+##    print(i)
+##else:
+##    print("pritam")
+##
+
+
+
+##
+##def joy():
+##    print("joyful")
+##
+##joy()
+##
+##
+##def func(name):
+##    print("high",name)
+##
+##func("pritam")
+##
+
+##
+##def si(p,t,r):
+##    s=(p*t*r)/100
+##    return s
+##
+##p=float(input("enter principle: "))
+##r=float(input("enter rate: "))
+##t=float(input("enter time in years: "))
+##print("the simple interest is:",si(p,t,r))
+##
+##
+##
+##
+####def sum(a,b):
+####    return(a+b)
+####
+####
+####print("the sum is:",sum(10,4))
+##
+##
+##def hh(name,message):
+##    print("printing the message with",name,"and",message)
+##
+##hh(name=(input("enter a name")),message=(input("enter a message")))
+##
+##
+##
+##
+##
+def si(p,t,r):
+    return((t*r)/p)
+
+print("the simple interest is:",si(t=10,r=10,p=1900))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
